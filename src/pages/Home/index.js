54beef556c7338e661e2af9e0b656f32f44ca81d
Vinicius Donadon <32843269+vbdonadon@users.noreply.github.com>
2020-02-13 16:51:40 -0300

    developed the css for the home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,96 @@
 import React from 'react';
 
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
 export default function Main() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-wish-masculino/12/COL-7158-012/COL-7158-012_zoom2.jpg?ims=326x"
+          alt="Tênis Adidas"
+        />
+
+        <strong>Tênis Adidas</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-wish-masculino/12/COL-7158-012/COL-7158-012_zoom2.jpg?ims=326x"
+          alt="Tênis Adidas"
+        />
+
+        <strong>Tênis Adidas</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-wish-masculino/12/COL-7158-012/COL-7158-012_zoom2.jpg?ims=326x"
+          alt="Tênis Adidas"
+        />
+
+        <strong>Tênis Adidas</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-wish-masculino/12/COL-7158-012/COL-7158-012_zoom2.jpg?ims=326x"
+          alt="Tênis Adidas"
+        />
+
+        <strong>Tênis Adidas</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-wish-masculino/12/COL-7158-012/COL-7158-012_zoom2.jpg?ims=326x"
+          alt="Tênis Adidas"
+        />
+
+        <strong>Tênis Adidas</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
